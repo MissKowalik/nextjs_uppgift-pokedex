799a815f12d01879a6c236b50c804dd94d5ad987
Linda <lindakowalik@gmail.com>
2025-08-27 14:30:12 +0200

    add render random Pokemon on button click

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Hero from "@/components/HeroHome";
 import SearchForm from "@/components/SearchForm";
 import FeaturedPokemons from "@/components/FeaturedPokemons";
 
@@ -6,18 +6,7 @@ export default function Home() {
   return (
     <main>
       {/* Hero */}
-      <section className="flex flex-col items-center gap-4 bg-gradient-to-br [background-image:linear-gradient(-10deg,_#C97FE4,_#AECDF6)] p-14">
-        <h1 className="text-center mt-14 text-8xl font-extrabold text-transparent bg-gradient-to-r from-purple-800 to-blue-800 [background-clip:text]">Gotta catch 'em all!</h1>
-        <p className="text-center text-white text-xl">Discover, search and explore the amazing world of Pokémon. Find<br /> your favourite and learn about their stats.</p>
-        <button className="btn-primary">
-          <Image
-            src="/Dice.svg"
-            width={25}
-            height={25}
-            alt="Dice"
-          />
-          Random Pokémon</button>
-      </section>
+      <Hero/>
 
       {/* Searchform component */}
       <SearchForm/>
