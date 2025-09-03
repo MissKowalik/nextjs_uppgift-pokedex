b01aab9200c4bcd5edb499abb88e6bf17ff4e6d6
Linda <lindakowalik@gmail.com>
2025-09-03 15:01:07 +0200

    added try..catch error handling on api calls

--- lib/data/Pokemon.ts
+++ lib/data/Pokemon.ts
@@ -4,19 +4,24 @@ import { Pokemon } from "../interfaces/Pokemon";
 
 
 export default async function getPokemonById(id: number) {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const data = await response.json();
-        
-        return {
-            id: data.id,
-            name: data.name,
-            image: data.sprites.other["official-artwork"].front_default,
-            types: data.types.map((t: PokeType) => t.type.name),
-            hp: data.stats[0].base_stat,
-            attack: data.stats[1].base_stat,
-            defense: data.stats[2].base_stat,
-        };
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    
+    return {
+        id: data.id,
+        name: data.name,
+        image: data.sprites.other["official-artwork"].front_default,
+        types: data.types.map((t: PokeType) => t.type.name),
+        hp: data.stats[0].base_stat,
+        attack: data.stats[1].base_stat,
+        defense: data.stats[2].base_stat,
+    };
+  } catch (error) {
+    console.log("Fetching failed:", error);
+  }
 }
+
 
 export async function pokeProps(id: number) {
     const pokemon = await getPokemonById(id);
@@ -25,26 +30,31 @@ export async function pokeProps(id: number) {
 
 
 export async function getAllPokemons(limit = 1025, skip = 0): Promise<Pokemon[]> {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${skip}&limit=${limit}`);
-  const data = await res.json();
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${skip}&limit=${limit}`);
+    const data = await res.json();
 
-  const results = await Promise.all(
-    data.results.map(async (p: PokemonListItem) => {
-      const detailsRes = await fetch(p.url);
-      const details = await detailsRes.json();
+    const results = await Promise.all(
+      data.results.map(async (p: PokemonListItem) => {
+        const detailsRes = await fetch(p.url);
+        const details = await detailsRes.json();
 
-      return {
-        id: details.id,
-        name: details.name,
-        image: details.sprites.other["official-artwork"].front_default,
-        types: details.types.map((t: PokeType) => t.type.name),
-        hp: details.stats[0].base_stat,
-        attack: details.stats[1].base_stat,
-        defense: details.stats[2].base_stat,
-      };
-    })
-  );
+        return {
+          id: details.id,
+          name: details.name,
+          image: details.sprites.other["official-artwork"].front_default,
+          types: details.types.map((t: PokeType) => t.type.name),
+          hp: details.stats[0].base_stat,
+          attack: details.stats[1].base_stat,
+          defense: details.stats[2].base_stat,
+        };
+      })
+    );
 
+    return results;
 
-  return results;
+  } catch (error) {
+    console.log("Fetching failed:", error);
+    return []
+  }
 }
